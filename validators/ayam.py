7558a9e3742fc0a4a280b611f68278109ac2a3a6
Skokoo <nourriwilliam789@gmail.com>
2026-07-02 17:45:36 +0700

    Update ayam.py

--- validators/ayam.py
+++ validators/ayam.py
@@ -1,21 +1,20 @@
 import sys
 from urllib.parse import urlparse, parse_qs
 
-def eye_detector(url):
-    parsed_url = urlparse(url)
-    queries = parse_qs(parsed_url.query)
-    
-    if not queries:
-        path_parts = [p for p in parsed_url.path.split('/') if p]
-        if path_parts:
-            print(f"PATH_PARAM|{parsed_url.path}")
+def ceker(laksasg):
+    hainanchicken = urlparse(laksasg)
+    charwayteow = parse_qs(hainanchicken.query)
+    if not charwayteow:
+        chilicrab = [f for f in hainanchicken.path.split('/') if f]
+        if chilicrab:
+            print(f"PATH_PARAM|{hainanchicken.path}")
         else:
             print("NO_PARAM|NONE")
         return
 
-    param_list = ",".join(queries.keys())
-    print(f"QUERY_PARAM|{parsed_url.path}|{param_list}")
+    kayatoast = ",".join(charwayteow.keys())
+    print(f"QUERY_PARAM|{hainanchicken.path}|{kayatoast}")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        eye_detector(sys.argv[1])
+        ceker(sys.argv[1])
