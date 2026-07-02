313a0ecd6b2ee0deef86d0582f36ff1f12c4ff53
Skokoo <nourriwilliam789@gmail.com>
2026-07-02 09:43:48 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -27,10 +27,9 @@ eexit() {
     if [[ "$AHAH" == "y" || "$AHAH" == "Y" ]]; then
         echo -e "\e[0;31m[-] Shutdown Triggered,\e[0m Clearing environment process tree.."
     (kill -9 -$$) 2>/dev/null
-trap 'eexit' SIGINT
     else
         echo -e "\e[0;32m[+] Resuming scan...\e[0m Continuing the multi vector synchronized strike."
-        trap 'exit' SIGINT
+        trap 'eexit' SIGINT
     fi
 }
 trap 'eexit' SIGINT
