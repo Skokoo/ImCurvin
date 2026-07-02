cea2dd299c94667083e6b131f48d31b8cefc9ce0
Skokoo <nourriwilliam789@gmail.com>
2026-07-02 18:47:25 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -11,16 +11,8 @@ source "$DEFIANCE_DIR/../tamper/hungry.sh"
 
 eexit() {
     trap - SIGINT SIGTERM EXIT
-    echo -e "\n\n\e[0;33m[\e[0m?\e[0;33m]\e[0m \e[1;33mAction Required:\e[0m Do you want to quit the session? (y/N): "
-    read -r -n 1 AHAH
-    echo "" 
-    if [[ "$AHAH" == "y" || "$AHAH" == "Y" ]]; then
-        echo -e "\e[0;31m[-] Shutdown Triggered,\e[0m Clearing environment process tree..."
-        (kill -9 -$$) 2>/dev/null
-    else
-        echo -e "\e[0;32m[+] Resuming scan...\e[0m Continuing the multi vector synchronized strike."
-        trap 'eexit' SIGINT
-    fi
+    echo -e "\n\n\e[0;33m[\e[0m?\e[0;33m]\e[0m \e[1;33m Interrupted, process killed. "
+        kill -9 -$$ 2>/dev/null
 }
 trap 'eexit' SIGINT
 
