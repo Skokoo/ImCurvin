b050abb45da26b0202e07a9bf2b24c8931979a76
Skokoo <nourriwilliam789@gmail.com>
2026-07-03 19:31:26 +0700

    Update defval.py

--- validators/defval.py
+++ validators/defval.py
@@ -6,7 +6,6 @@ import time
 # Copyright 2026 Skokoo
 # Licensed under the Apache License, Version 2.0
 # Ah man, i was tried while making this. i hope no bug bug.
-# There was a nonsense at the database checking, me lazy to fix it
 log_f = os.path.join(os.path.dirname(__file__), "../targetDef.log")
 
 def run_analysis():
