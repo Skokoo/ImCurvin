74b88467772111f5c92f69fab0bdcd1c77d85631
Skokoo <nourriwilliam789@gmail.com>
2026-07-03 14:30:44 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -142,8 +142,6 @@ vector_sqli_agressor_left() {
         final_query="${default_path}${defiance_tamper_path}"
 
         if [[ "$WORDLIST_MYSQL" == *"nonphp"* || "$WORDLIST_MYSQL" == *"HAHA"* ]]; then
-            final_query="${defiance_tamper_path}${query_payload}"
-        else
             final_query="${default_path}${defiance_tamper_path}${query_payload}"
         else
             if [[ "$defiance_tamper_path" == *"="* ]]; then
