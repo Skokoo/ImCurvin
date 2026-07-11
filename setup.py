581b13ee1f52ebd065579825631954b194ea46d7
Skokoo <nourriwilliam789@gmail.com>
2026-07-11 14:30:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="imcurvin",
-    version="1.2.4",
+    version="1.2.0",
     license="Apache 2.0",
     author="Skokoo",
     author_email="Skokoo@proton.me",
