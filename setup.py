5e3c5550381f4c7b69197a42f7ca2d726057b083
Skokoo <nourriwilliam789@gmail.com>
2026-07-11 13:57:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="imcurvin",
     version="1.2.1",
-    license='Apache 2.0',
+    license="Apache 2.0",
     author="Skokoo",
     author_email="Skokoo@proton.me",
     description="Web security auditing tool with advanced evasion modes",
