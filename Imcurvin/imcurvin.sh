e9d53e89b6884dcaf84264e80bf9adf0da292561
Skokoo <nourriwilliam789@gmail.com>
2026-08-01 22:01:27 +0700

    Update imcurvin.sh

--- Imcurvin/imcurvin.sh
+++ Imcurvin/imcurvin.sh
@@ -42,6 +42,7 @@ echo ""
 skip_confirm="false"
 nerf_mode="false"
 custom_cookie=""
+recon="true"
 enable_val="false"
 custom_wordlist=""
 target_url=""
@@ -51,6 +52,7 @@ while [[ "$#" -gt 0 ]]; do
     -u) target_url="$2"; shift 2 ;;
     -nerf) nerf_mode="true"; shift 1 ;;
     -val) enable_val="true"; shift 1 ;;
+    -rec) recon="true"; shift 1 ;;
     -cookie=*) custom_cookie="${1#*=}"; shift 1 ;;
     -cnf) skip_confirm="true"; shift 1 ;;
     -proxy=*) custom_proxy="${1#*=}"; shift 1 ;;
@@ -80,6 +82,7 @@ if [ -f "$script_dir/Defiancescan.sh" ]; then
   export nerf_mode
   export custom_cookie
   export enable_val
+  export recon
   source "$script_dir/Defiancescan.sh" "$target_url"
   echo ""
   exit 0
