f9628be5e470d8f4683203fa957fce5e4cce9041
Skokoo <nourriwilliam789@gmail.com>
2026-08-02 18:44:21 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -518,6 +518,7 @@ dork() {
   exit 0
 }
 show_tech() {
+echo -e "===========================================\n"
     echo -e "\n\e[34mReconnaissance\e[0m"
     echo -e "* \e[1mHybrid URL Parsing\e[0m: Dynamically traces \e[1mPreFlight HTTP Redirections\e[0m to secure the absolute destination URL before passing it to localized multi-vector attack sequences."
     echo -e ""
