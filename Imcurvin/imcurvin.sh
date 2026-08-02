cbf5261b236e2531cb18563fe3d6d151ee5760de
Skokoo <nourriwilliam789@gmail.com>
2026-08-02 17:01:51 +0700

    Update imcurvin.sh

--- Imcurvin/imcurvin.sh
+++ Imcurvin/imcurvin.sh
@@ -5,31 +5,10 @@
 # Licensed under the Apache License, Version 2.0
 # ==============================================
 
-  help() {
-  echo -e "->>\n"
-  echo -e "Usage: imcurvin -u <TARGET_URL> [OPTION]\n"
-  echo -e "Available Options:"
-  echo -e "  -u <URL>         : Specify the target website URL (Required)"
-  echo -e "  -cnf             : Automode (Passed directly to Defiance)"
-  echo -e "  -rec             : Run environment reconnaissance"
-  echo -e "  -shwpld          : Show payloads actively during execution"
-  echo -e "  -val             : Enable post-scan Python validation engine for latency isolation"
-  echo -e "  -cookie=<string> : Ingest custom session cookies (e.g., -cookie=\"PHPSESSID=123\")"
-  echo -e "  -proxy=<addr>    : Route traffic through a custom proxy (e.g., http://127.0.0.1:8080)"
-  echo -e "  -h               : Display this help guide"
-  echo -e "\n->>"
-  exit 0
-}
 terminate() {
   echo -e "\e[0;33m[\e[0m-\e[0;37m]\e[0m Execution failed for an unknown reason."
   exit 1
 }
-
-if ! command -v curl &> /dev/null; then
-  echo -e "\e[0;33m[\e[0m-\e[0;37m]\e[0m WARNING: 'curl' is not installed on your terminal."
-  echo -e "\e[0;33m[\e[0m-\e[0;37m]\e[0m Please install curl first before running imCurvin'."
-  terminate
-fi
 
 echo ""
 
@@ -54,13 +33,6 @@ while [[ "$#" -gt 0 ]]; do
     *) shift ;;
   esac
 done
-
-if [ -z "$target_url" ]; then
-  echo -e "\e[0;31m[\e[0m!\e[0;31m]\e[0m Error: URL not specified."
-  echo -e "\e[0;37m[\e[0mmi\e[0;37m]\e[0m Please refer to the option guide below:\n"
-  help
-  exit 1
-fi
 
 if [ -f "./Imcurvin/Defiancescan.sh" ]; then
   script_dir="$(pwd)/Imcurvin"
