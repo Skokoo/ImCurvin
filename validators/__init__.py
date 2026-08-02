f6b8680d874634032f6ba7c72d38905510f287dc
Skokoo <nourriwilliam789@gmail.com>
2026-08-02 13:52:59 +0700

    Update __init__.py

---
+++ validators/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2026 Skokoo
+# Licensed under the Apache License, Version 2.0
+#
+# I deeply hope this framework will thrive and reach its full potential 
+# as open-source engineers and contributors join our mission.
+pass
