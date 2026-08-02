261990a3ee62633ec8f0b74462ea636c039ee73b
Skokoo <nourriwilliam789@gmail.com>
2026-08-02 16:40:02 +0700

    Update Defiancescan.sh

--- Imcurvin/Defiancescan.sh
+++ Imcurvin/Defiancescan.sh
@@ -501,7 +501,23 @@ dork() {
           echo -e "\n[\033[34m$(date +%H:%M:%S)\033[0m] [i] Execution completed successfully."
           exit 0
         }
+  help() {
+  echo -e "->>\n"
+  echo -e "Usage: imcurvin -u <TARGET_URL> [OPTION]\n"
+  echo -e "Available Options:"
+  echo -e "  -u <URL>         : Specify the target website URL (Required)"
+  echo -e "  -cnf             : Automode (Passed directly to Defiance)"
+  echo -e "  -rec             : Run environment reconnaissance"
+  echo -e "  -shwpld          : Show payloads actively during execution"
+  echo -e "  -val             : Enable post-scan Python validation engine for latency isolation"
+  echo -e "  -cookie=<string> : Ingest custom session cookies (e.g., -cookie=\"PHPSESSID=123\")"
+  echo -e "  -proxy=<addr>    : Route traffic through a custom proxy (e.g., http://127.0.0.1:8080)"
+  echo -e "  -h               : Display this help guide"
+  echo -e "\n->>"
+  exit 0
+}
         print_defiance_logo
+        if [[ "$hell
         if [[ "$recon" = "true" ]]; then
           reconi
         fi
