a0ff7bf3f0da73ba0f2f2b29ecc801e02be69523
Skokoo <nourriwilliam789@gmail.com>
2026-08-09 16:23:42 +0700

    Update vector.sh

--- Imcurvin/vector.sh
+++ Imcurvin/vector.sh
@@ -70,7 +70,7 @@
         local current_method="${REQ_METHOD:-POST}"
 
         if [[ "$payloadsi" = "true" ]]; then
-          output_text="[\033[34m${current_time}\033[0m] [\e[0;34m<\e[0m] Vector 1 [${current_method}][PORT:$random_port] Param: $current_param \033[38;5;238m[Len: ${#active_payload}]\033[0m"
+          output_text="[\033[34m${current_time}\033[0m] [\e[0;34m<\e[0m] Vector 1 [${current_method}][PORT:$random_port] Param: $current_param \033[38;5;238m[Payload: ${active_payload}]\033[0m"
         else
           local technique_name="Generic Time-Based"
           case "${raw_payload}" in
