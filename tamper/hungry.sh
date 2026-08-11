66a4f49bab3ab4145813d8d6da24e30c0332c6da
Skokoo <nourriwilliam789@gmail.com>
2026-08-11 19:54:04 +0700

    Update hungry.sh

--- tamper/hungry.sh
+++ tamper/hungry.sh
@@ -1,63 +1,13 @@
 #!/bin/bash
 # Copyright 2026 Skokoo
 # Licensed under the Apache License, Version 2.0
-# This tamper should be used soon.
-# You can tell what tamper it is, just see the function name.
-# i like /**/
+
 space2comment_engine() {
     local bruh_serabii="$1" #food
     local bruh_cirengg=$(echo "$bruh_serabii" | sed 's/ /\/\*\*\//g' | sed 's/\+/\/\*\*\//g')
     echo "$bruh_cirengg" #road
 }
 
-appendnullbyte_engine() {
-    local bruh_putuu="$1"
-    local clean_payload=$(echo "$bruh_putuu" | sed 's/%00//g')
-    local bruh_duhduhduuudh="${clean_payload}%00"
-    echo "$bruh_duhduhduuudh"
-}
-# I really like this tamper, but i dont even see it is important.
-between_engine() {
-    local bruh_pempekk="$1" #rendang
-    if echo "$bruh_pempekk" | grep -qE "=[A-Z]"; then
-        local bruh_batagror="$bruh_pempekk"
-    else
-        local bruh_batagror=$(echo "$bruh_pempekk" | sed -E "s/\b([a-zA-Z0-9_'-]+)\s*=\s*([a-zA-Z0-9_'-]+)\b/\1 BETWEEN \2 AND \2/g")
-    fi
-    echo "$bruh_batagror"
-} #OH YEAH MAN BETWEEN I LIKE TAMPER... Uh wait, tempe?
-
-
-# Use this soon, but xor and b64 already make it good.
-charencode_engine() {
-    local bruh_baksow="$1"
-    local bruh_siomayy=""
-    local bruh_gettass=${#bruh_baksow}
-    local fa_gao=false
-
-    for (( i=0; i<bruh_gettass; i++ )); do
-        local bruh_kelepon="${bruh_baksow:$i:1}" # C language copy cat 
-        
-        if [[ "$bruh_kelepon" == "'" || "$bruh_kelepon" == '"' ]]; then
-            bruh_siomayy="${bruh_siomayy}${bruh_kelepon}"
-            if [ "$fa_gao" = true ]; then
-                fa_gao=false
-            else
-                fa_gao=true
-            fi
-            continue
-        fi
-
-        if [[ "$bruh_kelepon" =~ [a-zA-Z] ]] && [ "$fa_gao" = true ]; then
-            local bruh_getukk=$(printf "%d" "'$bruh_kelepon")
-            bruh_siomayy="${bruh_siomayy}CHAR(${bruh_getukk})"
-        else
-            bruh_siomayy="${bruh_siomayy}${bruh_kelepon}"
-        fi #phi
-    done
-    echo "$bruh_siomayy"
-}
-# Good for basic WAF.
 randomcase_engine() {
     local payload="$1"
     local result=""
@@ -77,12 +27,7 @@ randomcase_engine() {
     done
     echo "$result"
 }
-apostrophenullencode_engine() {
-    local payload="$1"
-    local obfuscated=$(echo "$payload" | sed "s/'/'%00/g")
-    echo "$obfuscated"
-}
-# last 3, i like this last 3. (except weird comment)
+
 xor_engine() {
     local input_str="$1"
     local key=137
@@ -97,23 +42,7 @@ xor_engine() {
     done
     echo -n "$output_hex"
 }
-# Used this soon? Hello??
-weirdcomment_engine() {
-    local input_str="$1"
-    local output_str=""
-    local trash_words=("Skokoo" "ImCurvin" "Defiance" "Hahaha" "LolEasy" "WafImHere")
-    
-    for (( i=0; i<${#input_str}; i++ )); do
-        local char="${input_str:$i:1}"
-        output_str="${output_str}${char}"
-        if [[ "$char" =~ [a-zA-Z0-9] ]] && (( RANDOM % 4 == 0 )); then
-            local random_word=${trash_words[$RANDOM % ${#trash_words[@]}]}
-            output_str="${output_str}/*${random_word}_$((RANDOM % 99))*/"
-        fi
-    done
-    echo "$output_str"
-}
-# This just to obvious, i wont explain it.
+
 base64_engine() {
     local wow="$1"
     echo -n "$wow" | xxd -r -p | base64 | tr -d '\n'
